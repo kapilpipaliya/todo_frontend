9c6f387b90c441e3af7ced9713653afdc59d16e9
Kapil <kapil.pipaliya@yahoo.com>
2019-12-25 08:40:03 +0530

    Fix Tab menu

--- src/components/MenuF.svelte
+++ src/components/MenuF.svelte
@@ -1,5 +1,8 @@
 <script>
+import * as R from 'ramda'
 export let menu = {}
+const h = R.compose(R.length, R.filter(x=>!x.hidden) )
+$: tabsCount = h(menu.tabs);
 </script>
 <div>
 {#if menu.title}
@@ -8,13 +11,15 @@ export let menu = {}
 <br>
 {#if menu.tabs}
 {#each menu.tabs as tab}
-	&nbsp&nbsp
+	{#if tabsCount > 1}&nbsp&nbsp{tab.title}:{/if}
 	<div>
 	{#each tab.columns as c}
+			{#if !tab.hidden}
 			&nbsp&nbsp
 			{#each c as u}
 				<a href={u.url}>{u.title}</a>{' '}
 			{/each}
+			{/if}
 	{/each}
 	</div>
 {/each}
