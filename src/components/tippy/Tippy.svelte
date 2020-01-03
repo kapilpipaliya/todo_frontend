9a6a4dcbc2f5aebb7183b45ca7254cd3db27c560
Kapil <kapil.pipaliya@yahoo.com>
2020-01-03 09:18:42 +0530

    Add Tippy.js Component

---
+++ src/components/tippy/Tippy.svelte
@@ -0,0 +1,18 @@
+<script>
+	import tippy from 'tippy.js';
+  import 'tippy.js/dist/tippy.css';
+  import { followCursor } from 'tippy.js';
+	
+ function mytippy(node, { props, plugins }) {
+  tippy(node, props, plugins);
+ }
+
+ let props = {
+    content: '<div class="tooltip">Styled tooltip text</div>',
+    onShow: function(instance) {
+      console.log(instance);
+    },
+  };
+ const plugins = [];
+</script>
+<span class='tippy-tooltip' use:mytippy={{ props, plugins }}>Hover Me</span>
---
+++ src/components/tippy/tippy.ts
@@ -0,0 +1,7 @@
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
+
+export default function(node, { props, plugins }) {
+  tippy(node, props, plugins);
+}
+// <span use:tippy={{ props, plugins }}>Hover Me</span>
