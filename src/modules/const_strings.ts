d9974b804909f337136d074305ac7ac587795f12
Kapil <kapil.pipaliya@yahoo.com>
2020-02-18 15:40:13 +0530

    make backend setting work without modifing.

--- src/modules/const_strings.ts
+++ src/modules/const_strings.ts
@@ -1,20 +1,12 @@
-const dev_conf = {
-  http_proto: 'http',
-  domain: 'localhost',
-  port: 8500,
-  ws_proto: 'ws',
+const backend_conf = {
+  http_proto: location.protocol,
+  domain: window.location.hostname,
+  port: location.protocol == 'http:' ? 8500 : 8501,
+  ws_proto: location.protocol == 'http:' ? 'ws' : 'wss'
 }
-const prod_conf = {
-  http_proto: 'https',
-  domain: 'scesoftwares.com',
-  port: 8501,
-  ws_proto: 'wss',
-}
-export const server = dev_conf
-
-export const product_img_url = `${server.http_proto}://${server.domain}:${server.port}/http/v1/user/download_id`
-export const thumb_url = `${server.http_proto}://${server.domain}:${server.port}/http/v1/user/thumb_id`
-export const ws_todo = `${server.ws_proto}://${server.domain}:${server.port}/todo`
+export const product_img_url = `${backend_conf.http_proto}://${backend_conf.domain}:${backend_conf.port}/http/v1/user/download_id`
+export const thumb_url = `${backend_conf.http_proto}://${backend_conf.domain}:${backend_conf.port}/http/v1/user/thumb_id`
+export const ws_todo = `${backend_conf.ws_proto}://${backend_conf.domain}:${backend_conf.port}/todo`
 
 
 
