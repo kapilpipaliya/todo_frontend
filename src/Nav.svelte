1a9c9fe3a7b9491a865fe00c9a7a6609df513ef9
Kapil <kapil.pipaliya@yahoo.com>
2020-02-19 22:35:37 +0530

    Refactor App.svelte file

---
+++ src/Nav.svelte
@@ -0,0 +1,49 @@
+<script lang='ts'>
+	import {current_member} from './modules/global_stores/current_member'
+	declare let $current_member
+
+	import MenuF from './components/UI/MenuF.svelte'
+
+	import { onMount, onDestroy, S, ws_connected, event_type as et,events as e, Unique } from './modules/index'
+	declare let $ws_connected
+
+	let mounted = false
+	let er = ''
+	let binded = false
+
+	const menu_evt = [et.get, e.my, e.form_schema_get, Unique.id ]
+	let menus  = []
+	onMount(() => {mounted = true})
+  	onDestroy(() => {S.unbind_([menu_evt]) })
+  	$: if (mounted) {if ($ws_connected) {er = ''; funcBindingOnce() } else {er = 'Reconnecting...'} }
+  	function funcBindingOnce() {
+	    if (!binded) {
+	      S.bind$(menu_evt, (d) => {
+	      	if(d[0].length && d[0][0]){
+	      		menus = d[0][0]
+	      	}
+	      }, 1)
+	      binded = true
+          S.trigger([ [menu_evt, ['menu']] ]) 
+	    }
+  	}
+
+	let email
+	$: email = $current_member?.email;
+</script>
+
+<nav>
+	Current User: {email}
+
+	{#if menus.navData}
+		<MenuF menu={menus.navData.account}/>
+	{/if}
+
+	{#if menus.navData}
+		<MenuF menu={menus.navData.global}/>
+	{/if}
+
+	{#if menus.navData}
+		<MenuF menu={menus.navData.admin}/>
+	{/if}
+</nav>
---
+++ src/components/UI/TopLevelComps.svelte
@@ -0,0 +1,27 @@
+<script lang='ts'>
+	import {maintenance} from '../../modules/global_stores/maintenance'
+	import {navigation} from '../../modules/global_stores/navigation'
+	import {current_time} from '../../modules/global_stores/time_store'
+
+	import Css from './Css.svelte'
+	
+	import ScrollTop from './ScrollTop.svelte'
+	import { NotificationDisplay } from '../index'
+
+	let n
+</script>
+
+<Css/>
+
+{#if $maintenance}
+	server is going down for sheduled maintenance
+{/if}
+
+{#if $navigation}
+	{navigation}
+{/if}
+
+<NotificationDisplay bind:this={n} />
+
+<ScrollTop/>
+{$current_time}
