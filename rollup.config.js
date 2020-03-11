0b999f089768dc0a39990e83941cc89a439a94ac
Kapil <kapil.pipaliya@yahoo.com>
2020-03-11 07:40:16 +0530

    rollup-plugin-bundle-html to replace-in-file

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import svelte from "rollup-plugin-svelte"
 import commonjs from "@rollup/plugin-commonjs"
 import resolve from "@rollup/plugin-node-resolve"
 //import serve from "rollup-plugin-serve" // use my own serve function
-import html from "rollup-plugin-bundle-html"
 //import css from "rollup-plugin-css-porter" // now use postcss
 import postcss from "rollup-plugin-postcss"
 import typescript from "rollup-plugin-typescript2"
@@ -15,14 +14,24 @@ import replace from '@rollup/plugin-replace';
 import { compress } from 'brotli'
 import gzipPlugin from 'rollup-plugin-gzip'
 import findFreePort from 'find-free-port-sync'
-let liveport = findFreePort({
+
+
+//import html from "rollup-plugin-bundle-html"
+import replaceInFile from 'replace-in-file'
+import path from 'path'
+import fs from 'fs'
+
+
+const liveport = findFreePort({
     start: 35000,
     end: 36000,
     num: 1,
     ip: 'localhost'
 })
-console.log(liveport)
-const hash = Date.now()
+let isLiveReload = true
+if(process.env.live == "no"){
+  isLiveReload = false
+}
 
 const extensions = [".ts", ".js"]
 
@@ -42,6 +51,39 @@ require('child_process').spawn('rm', ['-rf', dest], {
           shell: true
         })
 
+// const hash = crypto.createHash('md5').update(pkg.version).digest("hex").substr(0, 7);
+const hash = Date.now()
+// file: `public/bundles/bundle-${hash}.js`
+// Date.now()
+let htmlDone = false;
+const generateHtmlBuild = (options) => {
+    return {
+        async generateBundle(){
+            if (htmlDone) return;
+            const targDir = path.dirname(options.targ);
+            if (!fs.existsSync(targDir)){
+                fs.mkdirSync(targDir);
+            }
+            fs.writeFileSync(options.targ, fs.readFileSync(options.src));
+            const replaceOptions = {
+                files: options.targ,
+                from: /REPLACE_HASH/g,
+                to: hash,
+            };
+            try {
+                const results = await replaceInFile(replaceOptions)
+                console.log('Replacement results:', results);
+                htmlDone = true;
+            }
+            catch (error) {
+                console.error('Error occurred:', error);
+            }
+        }
+    };
+};
+
+
+
 const plugins = [
   //analyze(),
   replace({
@@ -53,11 +95,15 @@ const plugins = [
     preprocess: sveltePreprocessor(),
     emitCss: true,
   }),
-  html({
-    template: "index.html",
-    dest,
-    filename: "index.html",
-    absolute: true
+  // html({
+  //   template: "index.html",
+  //   dest,
+  //   filename: "index.html",
+  //   absolute: true
+  // }),
+  generateHtmlBuild({
+    src:  'index.html',
+    targ: `${dest}/index.html`
   }),
   postcss({
     extract: true
@@ -80,10 +126,10 @@ if (process.env.NODE_ENV === "development") {
     // In dev mode, call `npm run start` once
     // the bundle has been generated
     serve(),
+  );
     // Watch the `dist` directory and refresh the
     // browser on changes when not in production
-    livereload({ port: liveport, watch: dest })
-  );
+  if(isLiveReload) plugins.push(livereload({ port: liveport, watch: dest }))
 } else {
   plugins.push(terser({ sourcemap: true }))
 }
