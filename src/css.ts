18bfd23de1f555f8bd76639af41de4de22aad6ee
Kapil <kapil.pipaliya@yahoo.com>
2020-03-27 00:24:28 +0530

    use dynamic-import library

--- src/css.ts
+++ src/css.ts
@@ -1,14 +1,14 @@
 import { writable } from 'svelte/store'
 import { ET, E } from './enums'
 import { S } from './ws_events_dispatcher'
-import { style } from 'node_modules/dynamic-import/dist/import.js'
+import { style } from 'dynamic-import'
 export const css_frameworks = writable({
   bootstrap:
     '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">'
 })
 export const selected_frameworks = writable(['bootstrap'])
 export const css = writable({})
-const css_count_ = writable({ table: 0, normalize: 0, body: 1 })
+/*const css_count_ = writable({ table: 0, normalize: 0, body: 1 })
 export const css_count = {
   subscribe: css_count_.subscribe,
   increase: name => {
@@ -34,54 +34,40 @@ S.bind$(
   },
   1
 )
-
-/*
+*/
+export const css_loading = writable(false)
 let cssRaw = {}
 
-//https://github.com/brianleroux/loads-css/blob/master/es6.js
-function loadCSS(href, callback) {
-  var ss = window.document.createElement( "link" );
-  var ref = window.document.getElementsByTagName( "script" )[ 0 ];
-  var sheets = window.document.styleSheets;
-  ss.rel = "stylesheet";
-  ss.href = href;
-  // temporarily, set media to something non-matching to ensure it'll fetch without blocking render
-  ss.media = "only x";
-  // inject link
-  ref.parentNode.insertBefore( ss, ref );
-  // This function sets the link's media back to `all` so that the stylesheet applies once it loads
-  // It is designed to poll until document.styleSheets includes the new sheet.
-  function toggleMedia(){
-    var defined;
-    for( var i = 0; i < sheets.length; i++ ){
-      if( sheets[ i ].href && sheets[ i ].href.indexOf( href ) > -1 ){
-        defined = true;
-      }
-    }
-    if( defined ){
-      ss.media = "all";
-            callback()
-    }
-    else {
-      setTimeout( toggleMedia );
-    }
-  }
-  toggleMedia();
-  return ss;
-}
-
-let callbacks: Record<string, any> = {}
-
+let css_add_count = 0
 const css_count_ = { table: 0, normalize: 0, body: 1 }
 export const css_count = {
   increase: name => {
     css_count_[name] = (css_count_[name] || 0) + 1
-    if(!callbacks[name] && cssRaw[name]) callbacks[name] = loadCSS(cssRaw[name].link, ()=>0)
+    if (cssRaw[name] && css_count_[name] !== 0) {
+      css_loading.set(true)
+      ++css_add_count
+      console.log('css_add ', css_add_count, cssRaw[name].link)
+      style.import(cssRaw[name].link).then(function() {
+        --css_add_count
+        console.log('css_remove ', css_add_count, cssRaw[name].link)
+        if(css_add_count == 0) css_loading.set(false)
+        console.log('css files have been loaded!', cssRaw[name].link)
+      })
+    }
   },
   decrease: name => {
     setTimeout(_ => {
       css_count_[name] = css_count_[name] - 1
-      if(css_count_[name] == 0 && callbacks[name])  callbacks[name].remove()
+      if (css_count_[name] == 0) {
+        ++css_add_count
+        console.log('css_add ', css_add_count, cssRaw[name].link)
+        style.unload(cssRaw[name].link).then(function() {
+          --css_add_count
+          console.log('css_remove ', css_add_count, cssRaw[name].link)
+          if(css_add_count == 0) css_loading.set(false)
+          console.log('css files have been unloaded!', cssRaw[name].link)
+        })
+      }
     }, 2000)
   }
 }
@@ -89,20 +75,34 @@ export const css_count = {
 // {"table":{"classes":{},"link":"/table.5efd916f.css"}}
 S.bind$(
   [ET.get, E.css_event, 0],
-  function(data:{"table":{link: ""}}) {
+  function(data: { table: { link: '' } }) {
+    css_loading.set(true)
     css.set(data)
     for (let [k, v] of Object.entries(data)) {
-      if(cssRaw[k]?.link !== v.link){
-         if(css_count_[k] > 0) {
-           if(callbacks[k]) {
-             callbacks[k].remove()
-           }
-           callbacks[k] = loadCSS(v.link, ()=>0)
-         }
+      if (cssRaw[k]?.link !== v.link) {
+        if (css_count_[k] > 0) {
+          if(cssRaw[k]?.link) {
+            ++css_add_count
+            console.log('css_add ', css_add_count, cssRaw[k].link)
+            style.unload(cssRaw[k].link).then(function() {
+              --css_add_count
+              console.log('css_remove ', css_add_count, cssRaw[k].link)
+              if(css_add_count == 0) css_loading.set(false)
+              console.log('css files have been unloaded when setting data!')
+            })
+          }
+          ++css_add_count
+          console.log('css_add ', css_add_count, v.link)
+          style.import(v.link).then(function() {
+            --css_add_count
+            console.log('css_remove ', css_add_count, v.link)
+            if(css_add_count == 0) css_loading.set(false)
+            console.log('css files have been loaded when setting data!', v.link)
+          })
+        }
       }
     }
-    cssRaw=data
+    cssRaw = data
   },
   1
 )
-*/
